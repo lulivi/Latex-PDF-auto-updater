3cf710391fc0a75c193517f9323a07ba68177ae5
lulivi <luislivilla@gmail.com>
2017-07-01 00:22:48 +0200

    Created repository

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# latex_pdf_auto_updater
+# Latex PDF auto updater
+
+## Installation
+
+You can clone this repository with the command below
+```
+git clone git@github.com:lulivi/Latex-PDF-auto-updater.git
+```
+or just click on the "Download as zip" button.
+
+## Usage
+
+You have to execute this script from the directory in wich you have your Latex files. Simply run:
+```bash
+./updatePdfLatex.sh <output_directory_of_auxiliar_files> <target_file.pdf>
+```
+After this, you can edit your tex files and let the script update the pdf.
+
+## References
+
+I got the idea of this script mainly from two blog entries made by [Elbaulp](https://github.com/Elbaulp):
+
+- [Compilar automáticamente ficheros en latex mientras los modificamos](https://elbauldelprogramador.com/compilar-automaticamente-ficheros-en-latex-mientras-los-modificamos/).
+- [Ejecutar un script al modificar un fichero con inotify](https://elbauldelprogramador.com/ejecutar-un-script-al-modificar-un-fichero-con-inotify).
+
+## Contact
+
+You can contact me in luislivilla`at`gmail.com or directly with [Telegram](t.me/lulivi).
+
+## License
+
+Checkout [LICENSE](https://github.com/lulivi/Latex-PDF-auto-updater/blob/master/LICENSE)
---
+++ updatePdfLatex.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+###############################################################################
+# FILENAME :
+#   updatePdfLatex.sh
+#
+# DESCRIPTION :
+#   Updates the pdf in a temporal directory and brings it back to
+#   main directory.
+#
+# AUTHOR :
+#   Copyright 2017, Luis Liñán (luislivilla at gmail.com)
+#
+# REPOSITORY :
+#   https://github.com/lulivi/Latex-PDF-auto-updater
+#
+# LICENSE :
+#   This program is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License
+#   as published by the Free Software Foundation, version 3.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>
+###############################################################################
+
+function help {
+  echo "$0 <directory_to_watch> <file_to_watch>"
+}
+
+if [ "$#" -ne 2 ]; then
+  help
+  exit 1
+fi
+
+# Copy the pdf from the temporal directory to the parent directory
+function listen_pdf_update {
+  while true; do
+    change=$(inotifywait -e close_write $1)
+    echo "=================================="
+    echo "$change"
+    change=${change##* }
+    if [ "$change" = "$2" ]; then
+      cp $1/*.pdf ./
+      echo "===> Moved pdf to parent directory"
+    fi
+    echo "=================================="
+  done
+}
+
+# Set up listener for the target PDF file
+listen_pdf_update $1 $2 &
+
+# Set up latex listener for changes in the directory
+latexmk -shell-escape -xelatex -pdf -pvc -output-directory=$1
+
+# Kill all processes created in this script
+kill -9 -$$
